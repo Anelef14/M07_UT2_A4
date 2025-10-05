5d0840e6cad6a1576829aee7c22b6705f6029390
Anastasia <anelef14@gmail.com>
2025-10-05 21:39:14 +0200

    Add the links to every restaurant

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
+<?php
+$restaurants = [
+"Restaurant1",
+"Restaurant2",
+"Restaurant3",
+"Restaurant4"
+];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Restaurants List</title>
 </head>
 <body>
-    
-</body>
+    <h1>Restaurants</h1>
+
+    <ul>
+        <?php foreach ($restaurants as $restaurant): ?>
+        <li><a href="reservation.php?restaurant=<?php echo urlencode($restaurant); ?>"><?php echo $restaurant; ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+
+    <p><a href="reservation.php">Reserve at another restaurant</a></p></body>
 </html>
